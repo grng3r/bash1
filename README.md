d7c5525d6ddb52b841f3c2d5781bd86efd01f1b9
gr@ng3r <grng3r@morty>
2019-12-08 14:56:42 +0000

    welcome&readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a collection of either useful software or things that I found helpful in some way developed in bash on Ubuntu some of it may be later translted to python scripts
---
+++ welcome.sh
@@ -0,0 +1,16 @@
+################################
+#    Welcome message          #
+###############################
+echo "Oh Jeez! Oh Man!" | toilet -f smslant -k --metal
+echo "$(date '+%d %m %Y %T' | toilet -f term -F border --gay)"
+who
+free -g
+ps
+mpstat
+head -3 /var/log/auth.log
+vmstat 
+ipcs --human
+netstat -ern
+netstat -i
+netstat -ta
+
